653ed49146bbe813596486550225d497541ad8d7
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-09-07 15:20:07 -0400

    Add loading screen

--- src/pages/ItemDetails.js
+++ src/pages/ItemDetails.js
@@ -8,18 +8,21 @@ import { ItemsContext } from "../contexts/items-contexts";
 const ItemDetails = () => {
   const { itemId } = useParams();
   const [item, setItem] = useState([]);
+  const [loadingDetails, setLoadingDetails] = useState(false);
   const [error, setError] = useState(false);
   const navigate = useNavigate();
   const { cartItems, setCartItems } = useContext(ItemsContext);
 
   useEffect(() => {
     const fetchItem = async () => {
+      setLoadingDetails(true);
       try {
         const response = await fetch(
           `https://items-rendergui.herokuapp.com/items/${itemId}`
         );
         const data = await response.json();
         setItem(data);
+        setLoadingDetails(false);
       } catch (err) {
         console.log(err);
       }
@@ -49,23 +52,30 @@ const ItemDetails = () => {
   };
 
   return (
-    <div className={classes.detailsContainer}>
-      <FontAwesomeIcon className={classes.detailsImage} icon={faShirt} />
-      <h2 className={classes.detailsItem}>{item.itemName}</h2>
-      <p className={classes.detailsDescription}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation.
-      </p>
-      <h3 className={classes.detailsPrice}>${item.price}</h3>
-      <button
-        className={classes.addToCartButton}
-        type="button"
-        onClick={addToCartHandler}
-      >
-        Add to cart
-      </button>
-      {error && <p className={classes.errorMessage}>Item already in cart!</p>}
+    <div>
+      {loadingDetails && (
+        <div className={classes.loadingDetailsContainer}>
+          <p>Loading...</p>
+        </div>
+      )}
+      <div className={classes.detailsContainer}>
+        <FontAwesomeIcon className={classes.detailsImage} icon={faShirt} />
+        <h2 className={classes.detailsItem}>{item.itemName}</h2>
+        <p className={classes.detailsDescription}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation.
+        </p>
+        <h3 className={classes.detailsPrice}>${item.price}</h3>
+        <button
+          className={classes.addToCartButton}
+          type="button"
+          onClick={addToCartHandler}
+        >
+          Add to cart
+        </button>
+        {error && <p className={classes.errorMessage}>Item already in cart!</p>}
+      </div>
     </div>
   );
 };
--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,19 +1,22 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import ItemCard from "../components/ItemCard";
 import { ItemsContext } from "../contexts/items-contexts";
 import classes from "./Items.module.scss";
 
 const Items = () => {
   const { items, setItems } = useContext(ItemsContext);
+  const [loadingItems, setLoadingItems] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoadingItems(true);
       try {
         const response = await fetch(
           "https://items-rendergui.herokuapp.com/items"
         );
         const data = await response.json();
         setItems(data);
+        setLoadingItems(false);
       } catch (err) {
         console.log(err);
       }
@@ -23,17 +26,24 @@ const Items = () => {
   }, [setItems]);
 
   return (
-    <div className={classes.itemsContainer}>
-      {items.map((item) => {
-        return (
-          <ItemCard
-            key={item.id}
-            itemName={item.itemName}
-            price={item.price}
-            itemId={item.id}
-          />
-        );
-      })}
+    <div>
+      {loadingItems && (
+        <div className={classes.loadingItemsContainer}>
+          <p>Loading...</p>
+        </div>
+      )}
+      <div className={classes.itemsContainer}>
+        {items.map((item) => {
+          return (
+            <ItemCard
+              key={item.id}
+              itemName={item.itemName}
+              price={item.price}
+              itemId={item.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
